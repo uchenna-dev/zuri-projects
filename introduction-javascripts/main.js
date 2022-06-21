8ebf14127ba78b7989b8fb6d12762c73a7e0f354
Empress <101710164+uchenna-dev@users.noreply.github.com>
2022-06-21 11:56:52 +0100

    javascript introduction

---
+++ introduction-javascripts/main.js
@@ -0,0 +1,8 @@
+let first_name = "Victoria";
+let last_name = " Ike";
+let height = " 5'11inch";
+let country = " Nigeria";
+
+console.log(
+  "Hello" + first_name + " " + last_name + " " + height + " " + country
+);
